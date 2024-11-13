ed3174ffb43d745e46bc9e39e8e5e05fc90e5432
rafia02 <rafiabinterashed01@gmail.com>
2024-11-13 22:32:48 +0600

    dashboard overview

--- src/components/shared/admindashboardSidebar.tsx
+++ src/components/shared/admindashboardSidebar.tsx
@@ -1,38 +1,95 @@
+"use client"
 import { CalendarIcon, CogIcon, HomeIcon, UserGroupIcon } from "@heroicons/react/16/solid"
 import Link from "next/link"
+import { usePathname } from "next/navigation";
 
 const AdminDashboardSidebar = ({isSidebarOpen}: {isSidebarOpen: boolean}) => {
+  const router = usePathname();
+  const isActive = (path: string) => router === path;
   return (
 
-
-
-    <div  className={`fixed lg:static inset-y-0 left-0 transform ${
-      isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-    } lg:translate-x-0 w-72  bg-[#435b98] text-white p-5 transition-transform duration-200 ease-in-out z-10`}>
+<div
+      className={`fixed lg:static inset-y-0 left-0 transform ${
+        isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+      } lg:translate-x-0 w-72 bg-[#435b98] text-white p-5 transition-transform duration-200 ease-in-out z-10`}
+    >
       <h2 className="text-2xl font-semibold p-2 mb-6">Doctors Portal</h2>
       <nav className="flex flex-col gap-4">
-        <Link href="/dashboard" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+        <Link
+          href="/dashboard"
+          className={`flex items-center p-2 rounded ${
+            isActive("/dashboard") ? "bg-[#4967b3]" : "hover:bg-[#4967b3]"
+          }`}
+        >
           <HomeIcon className="w-6 h-6 mr-2" />
-          Dashboard Home
+          Dashboard overview
         </Link>
-        <Link href="/dashboard/appointments" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+        <Link
+          href="/dashboard/appointments"
+          className={`flex items-center p-2 rounded ${
+            isActive("/dashboard/appointments") ? "bg-[#4967b3]" : "hover:bg-[#4967b3]"
+          }`}
+        >
           <CalendarIcon className="w-6 h-6 mr-2" />
           Appointments
         </Link>
-        <Link href="/dashboard/doctors" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+        <Link
+          href="/dashboard/doctors"
+          className={`flex items-center p-2 rounded ${
+            isActive("/dashboard/doctors") ? "bg-[#4967b3]" : "hover:bg-[#4967b3]"
+          }`}
+        >
           <UserGroupIcon className="w-6 h-6 mr-2" />
           Doctors
         </Link>
-        <Link href="/dashboard/patients" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+        <Link
+          href="/dashboard/patients"
+          className={`flex items-center p-2 rounded ${
+            isActive("/dashboard/patients") ? "bg-[#4967b3]" : "hover:bg-[#4967b3]"
+          }`}
+        >
           <UserGroupIcon className="w-6 h-6 mr-2" />
           Patients
         </Link>
-        <Link href="/dashboard/settings" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+        <Link
+          href="/dashboard/settings"
+          className={`flex items-center p-2 rounded ${
+            isActive("/dashboard/settings") ? "bg-[#4967b3]" : "hover:bg-[#4967b3]"
+          }`}
+        >
           <CogIcon className="w-6 h-6 mr-2" />
           Settings
         </Link>
       </nav>
     </div>
+
+    // <div  className={`fixed lg:static inset-y-0 left-0 transform ${
+    //   isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+    // } lg:translate-x-0 w-72  bg-[#435b98] text-white p-5 transition-transform duration-200 ease-in-out z-10`}>
+    //   <h2 className="text-2xl font-semibold p-2 mb-6">Doctors Portal</h2>
+    //   <nav className="flex flex-col gap-4">
+    //     <Link href="/dashboard" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+    //       <HomeIcon className="w-6 h-6 mr-2" />
+    //       Dashboard overview
+    //     </Link>
+    //     <Link href="/dashboard/appointments" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+    //       <CalendarIcon className="w-6 h-6 mr-2" />
+    //       Appointments
+    //     </Link>
+    //     <Link href="/dashboard/doctors" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+    //       <UserGroupIcon className="w-6 h-6 mr-2" />
+    //       Doctors
+    //     </Link>
+    //     <Link href="/dashboard/patients" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+    //       <UserGroupIcon className="w-6 h-6 mr-2" />
+    //       Patients
+    //     </Link>
+    //     <Link href="/dashboard/settings" className="flex items-center hover:bg-[#4967b3] p-2 rounded">
+    //       <CogIcon className="w-6 h-6 mr-2" />
+    //       Settings
+    //     </Link>
+    //   </nav>
+    // </div>
   )
 }
 
