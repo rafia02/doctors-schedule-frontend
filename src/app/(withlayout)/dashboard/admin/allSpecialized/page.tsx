80c94b680af732cde07f642d35ac90cc0365a912
rafia02 <rafiabinterashed01@gmail.com>
2024-12-24 22:40:33 +0600

    all specialized

--- src/app/(withlayout)/dashboard/admin/allSpecialized/page.tsx
+++ src/app/(withlayout)/dashboard/admin/allSpecialized/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { EnvelopeIcon, PhoneIcon, StarIcon, UserIcon } from "@heroicons/react/16/solid";
+import { EnvelopeIcon, PhoneIcon, StarIcon, TrashIcon, UserIcon } from "@heroicons/react/16/solid";
+import Image from 'next/image';
 const AllSpecialized = () => {
     const data = [
         { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin', specialty: 'Cardiology', phone: '123-456-7890', rating: 4.8, father: 'abc', mather: 'zyx', brother: 'abdul jamal', sister: 'mitu kabir' },
@@ -18,11 +19,12 @@ const AllSpecialized = () => {
               <thead className="bg-primary text-white rounded-lg text-xs uppercase ">
                 <tr>
                   <th scope="col" className="px-7 py-4">N.o</th>
-                  <th scope="col" className="px-8 py-4">Name</th>
-                  <th scope="col" className="px-8 py-4">Specialty</th>
-                  <th scope="col" className="px-8 py-4">Email</th>
+                  <th scope="col" className="px-8 py-4">Image</th>
+                  <th scope="col" className="px-8 py-4">Department</th>
+                  <th scope="col" className="px-8 py-4">Action</th>
+                  {/* <th scope="col" className="px-8 py-4">Email</th>
                   <th scope="col" className="px-8 py-4">Phone</th>
-                  <th scope="col" className="px-8 py-4 rounded-tr-lg">Rating</th>
+                  <th scope="col" className="px-8 py-4 rounded-tr-lg">Rating</th> */}
                 </tr>
               </thead>
               <tbody>
@@ -31,36 +33,17 @@ const AllSpecialized = () => {
                     <td className="whitespace-nowrap px-7 py-6">{index + 1}</td>
 
                     <td className="whitespace-nowrap px-8 py-6">
-                      <div className="flex items-center gap-1">
-                        <UserIcon className="w-4 h-4 text-primary" />
-                        <p>{item.name}</p>
-                      </div>
+                      <Image src="https://res.cloudinary.com/doctorsshedule/image/upload/v1734892754/kdob499vtqkd1k6hmkhd.png" height={50} width={50} alt=''></Image>
                     </td>
-
-                    <td className="whitespace-nowrap px-8 py-6">{item.specialty}</td>
+                    <td className="whitespace-nowrap px-8 py-6">Eye Care</td>
 
                     <td className="whitespace-nowrap px-8 py-6">
-                      <div className="flex items-center gap-2">
-                        <EnvelopeIcon className="w-4 h-4 text-primary" />
-                        <p>{item.email}</p>
-                      </div>
+                        <TrashIcon className='w-5 text-red-500'></TrashIcon>
                     </td>
 
+        
 
-                    <td className="whitespace-nowrap px-8 py-6">
-                      <div className="flex items-center gap-1">
-                        <PhoneIcon className="w-4 h-4 text-primary" />
-                        <p>{item.phone}</p>
-                      </div>
-                    </td>
-
-
-                    <td className="whitespace-nowrap px-8 py-6">
-                      <div className="flex items-center gap-1">
-                        <StarIcon className="w-4 h-4 text-primary" />
-                        <p>{item.rating}</p>
-                      </div>
-                    </td>
+                    
 
 
                   </tr>
